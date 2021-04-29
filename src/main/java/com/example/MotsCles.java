b8a7699891c9c9cb6d08427542abe8641816bbe9
l3miage-mounirr <77291972+l3miage-mounirr@users.noreply.github.com>
2021-04-29 16:30:30 +0200

    Create MotsCles.java

---
+++ src/main/java/com/example/MotsCles.java
@@ -0,0 +1,27 @@
+
+package com.example;
+
+public class MotsCles {
+       
+    private String defisId;
+    private String motCle ;
+
+    public String getDefisId() {
+        return defisId;
+    }
+
+    public String getMotCle() {
+        return motCle;
+    }
+
+    public void setDefisId(String defisId) {
+        this.defisId = defisId;
+    }
+
+    public void setMotCle(String motCle) {
+        this.motCle = motCle;
+    }
+    
+    
+
+}
